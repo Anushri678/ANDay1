446db7ea56144e5dd2027c2059a26fbad654ef16
Hp <Hp@DESKTOP-T50AIUU>
2022-08-08 21:47:59 +0530

    first commit

---
+++ FirstProject/src/customerview/Main.java
@@ -0,0 +1,48 @@
+package customerview;
+
+import customermodel.Customer;
+import customermodel.Product;
+
+public class Main {
+	
+	int getcustomerIdFromCustomer(Customer custObj) {
+		return(custObj.getCustomerId());
+	}
+	
+	void getcustomerIdFromCustomer1(Customer custObj) {
+		String custName = custObj.getCustomerName();
+		System.out.println("Customer Name : " + custName);
+	}
+	
+	String getproductNameFromProduct(Product proObj) {
+		return(proObj.getProductName());
+	}
+	
+	
+    int getPriceFromProduct(Product proObj) {
+    	return(2*proObj.getPrice());
+    }
+
+	public static void main(String args[]) {
+    	Main mainObj = new Main();
+
+		Customer customer1 = new Customer(1,"Anu");
+		int custId = mainObj.getcustomerIdFromCustomer(customer1); 
+		System.out.println("Customer Id : "+custId);
+		
+		mainObj.getcustomerIdFromCustomer1(customer1);
+		
+		Product product1 = new Product(2, "Rice", 100);
+		String proName = mainObj.getproductNameFromProduct(product1);
+		System.out.println("Product Name : " + proName);
+		
+		
+		int proPrice = mainObj.getPriceFromProduct(product1);
+		System.out.println("Product Price : " + proPrice);
+		
+		
+		
+    	
+    }
+
+}
